95c0070c2751459e821ea55d50f8b59c7a0880b6
HappyBrother2022 <happy.brother2022@gmail.com>
2022-09-02 08:27:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## covid19-statistics-dashboard
+A simple map Covid-19 statistics dashboard made with React
+![](https://cdn.discordapp.com/attachments/873441703330185250/932587235834994698/Screenshot_2022-01-17_18-47-39.png)
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header>
+            <div className="container container-content">
+                <h1>Coronavirus (COVID-19)</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
